2e38955992c56091758a1e892b0ef525d7498ac6
Joshua Uwagwu <chuksjosh20@gmail.com>
2022-05-06 17:59:50 +0100

    added category controller

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,7 +1,10 @@
+using BulkyBookWeb.Extentions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.ConfigureServices(builder.Configuration);
 
 var app = builder.Build();
 
